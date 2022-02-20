ec250fa78d8e5537e8d1f2126c1ec25dee4ec029
pw <1040479402@qq.com>
2022-02-20 15:14:54 +0800

    1

---
+++ react/react_staging/src/App.js
@@ -0,0 +1,14 @@
+//创建“外壳”组件App
+import React,{Component} from "react"
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                Hello ,react!
+            </div>
+        )
+    }
+}
+//暴露App组件
+export default App
