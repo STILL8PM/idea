722ed48d9dd3a53e195c0f83aed1a65c926e8a44
pw <1040479402@qq.com>
2022-02-21 19:25:01 +0800

    20220221

---
+++ react/react_staging/src/component/List/index.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import './index.css'
+
+export default class List extends Component {
+    render() {
+        return (
+            <div className="row">
+                <div className="card">
+                    <a href="https://github.com/reactjs" target="_blank" rel='noreferrer'>
+                        <img src="https://tse1-mm.cn.bing.net/th/id/OIP-C.ZeQ5h5qmFJdYmGKtrR-I9gAAAA?w=187&h=187&c=7&r=0&o=5&pid=1.7" style={{ width: '100px' }} alt='Head portrait'/>
+                    </a>
+                    <p className="card-text">reactjs</p>
+                </div>
+                <div className="card">
+                    <a href="https://github.com/reactjs" target="_blank" rel='noreferrer'>
+                        <img src="https://tse1-mm.cn.bing.net/th/id/OIP-C.ZeQ5h5qmFJdYmGKtrR-I9gAAAA?w=187&h=187&c=7&r=0&o=5&pid=1.7" style={{ width: '100px' }} alt='Head portrait'/>
+                    </a>
+                    <p className="card-text">reactjs</p>
+                </div>
+                <div className="card">
+                    <a href="https://github.com/reactjs" target="_blank" rel='noreferrer'>
+                        <img src="https://tse1-mm.cn.bing.net/th/id/OIP-C.ZeQ5h5qmFJdYmGKtrR-I9gAAAA?w=187&h=187&c=7&r=0&o=5&pid=1.7" style={{ width: '100px' }} alt='Head portrait'/>
+                    </a>
+                    <p className="card-text">reactjs</p>
+                </div>
+                <div className="card">
+                    <a href="https://github.com/reactjs" target="_blank" rel='noreferrer'>
+                        <img src="https://tse1-mm.cn.bing.net/th/id/OIP-C.ZeQ5h5qmFJdYmGKtrR-I9gAAAA?w=187&h=187&c=7&r=0&o=5&pid=1.7" style={{ width: '100px' }} alt='Head portrait'/>
+                    </a>
+                    <p className="card-text">reactjs</p>
+                </div>
+                <div className="card">
+                    <a href="https://github.com/reactjs" target="_blank" rel='noreferrer'>
+                        <img src="https://tse1-mm.cn.bing.net/th/id/OIP-C.ZeQ5h5qmFJdYmGKtrR-I9gAAAA?w=187&h=187&c=7&r=0&o=5&pid=1.7" style={{ width: '100px' }} alt='Head portrait'/>
+                    </a>
+                    <p className="card-text">reactjs</p>
+                </div>
+            </div>
+        )
+    }
+}
