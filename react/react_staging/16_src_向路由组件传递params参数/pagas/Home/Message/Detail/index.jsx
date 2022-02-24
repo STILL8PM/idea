d93fde12163ba553f73d393fe3fb9bfed1b64bca
pw <1040479402@qq.com>
2022-02-24 21:42:32 +0800

    20220224

--- react/react_staging/16_src_向路由组件传递params参数/pagas/Home/Message/Detail/index.jsx
+++ react/react_staging/16_src_向路由组件传递params参数/pagas/Home/Message/Detail/index.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import qs from "query-string"
 
 const DetailData = [
     { id: '01', title: '消息1', content: '你好,中国' },
@@ -8,22 +7,15 @@ const DetailData = [
 ]
 export default class Detail extends Component {
     render() {
-        console.log(this.props);
-        // const { id, title } = this.props.match.params//接收params参数
-
-        //接收search参数
-        const {search}  = this.props.location
-        const result  = qs.parse(search)
-        console.log(result);
-
-        /*   const findResult = DetailData.find((detailObj) => {
+        const { id, title } = this.props.match.params
+        const findResult = DetailData.find((detailObj) => {
             return detailObj.id === id
-          }) */
+        })
         return (
             <ul>
-                {/*  <li>ID:{id}</li>
+                <li>ID:{id}</li>
                 <li>TITLE:{title}</li>
-                <li>CONTENT:{findResult.content}</li> */}
+                <li>CONTENT:{findResult.content}</li>
             </ul>
         )
     }
--- react/react_staging/16_src_向路由组件传递params参数/pagas/Home/Message/index.jsx
+++ react/react_staging/16_src_向路由组件传递params参数/pagas/Home/Message/index.jsx
@@ -18,21 +18,15 @@ export default class Message extends Component {
                         this.state.messageArr.map((msgObj) => {
                             return (
                                 <li key={msgObj.id}>
-                                    {/* 向路由组件传递params参数 */}
-                                    {/* <Link to={`/home/message/detail/${msgObj.id}/${msgObj.title}`}>{msgObj.title}</Link>&nbsp;&nbsp; */}
-
-                                    {/* 向路由组件传递search参数 */}
-                                    <Link to={`/home/message/detail/?id=${msgObj.id}$title=${msgObj.title}`}>{msgObj.title}</Link>&nbsp;&nbsp;
+                                {/* 向路由组件传递params参数 */}
+                                    <Link to={`/home/message/detail/${msgObj.id}/${msgObj.title}`}>{msgObj.title}</Link>&nbsp;&nbsp;
                                 </li>
                             )
                         })
                     }
                 </ul><hr />
                 {/* 声明接收params参数 */}
-                {/* <Route path='/home/message/detail/:id/:title'  component={Detail} /> */}
-
-                {/* search参数无需声明接收，正常注册路由即可 */}
-                <Route path='/home/message/detail/'  component={Detail} />
+                <Route path='/home/message/detail/:id/:title'  component={Detail} />
             </div>
         )
     }
