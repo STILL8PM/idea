55c352fb60e88bc8b280146bd860828dff8d4ddc
pw <1040479402@qq.com>
2022-02-24 17:39:59 +0800

    20220224

---
+++ react/react_staging/src/pagas/Home/Message/Detail/index.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+const DetailData = [
+    { id: '01', title: '消息1', content: '你好,中国' },
+    { id: '02', title: '消息2', content: '你好,尚硅谷' },
+    { id: '03', title: '消息3', content: '你好,React' },
+]
+export default class Detail extends Component {
+    render() {
+        const { id, title } = this.props.match.params
+        const findResult = DetailData.find((detailObj) => {
+            return detailObj.id === id
+        })
+        return (
+            <ul>
+                <li>ID:{id}</li>
+                <li>TITLE:{title}</li>
+                <li>CONTENT:{findResult.content}</li>
+            </ul>
+        )
+    }
+}
