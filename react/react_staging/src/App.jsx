7ebb3c573ecca93486b31c798e9a93a44ce214ad
pw <1040479402@qq.com>
2022-02-25 17:42:47 +0800

    20220225

--- react/react_staging/src/App.jsx
+++ react/react_staging/src/App.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { DatePicker, Button } from 'antd';
 import { WechatOutlined, WeiboOutlined, SearchOutlined } from '@ant-design/icons';
-import 'antd/dist/antd.css';
 const { RangePicker } = DatePicker;
 
 export default class App extends Component {
