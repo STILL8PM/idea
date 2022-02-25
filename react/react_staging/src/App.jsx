5a97c6301c2546496f33b6386c54aadd9da94789
pw <1040479402@qq.com>
2022-02-25 17:17:36 +0800

    20220225

--- react/react_staging/src/App.jsx
+++ react/react_staging/src/App.jsx
@@ -1,39 +1,27 @@
 import React, { Component } from 'react'
-import { Route, Switch,Redirect} from 'react-router-dom'
-import Header from './component/Header'//一般组件
-import MyNavLink from './component/MyNavLink'//一般组件
-import About from './pagas/About'//路由组件
-import Home from './pagas/Home'//路由组件
+import { DatePicker, Button } from 'antd';
+import { WechatOutlined, WeiboOutlined, SearchOutlined } from '@ant-design/icons';
+import 'antd/dist/antd.css';
+const { RangePicker } = DatePicker;
 
 export default class App extends Component {
+	onChange = (date, dateString) => {
+		console.log(date, dateString);
+		return 
+	}
 	render() {
 		return (
 			<div>
-				<div className="row">
-					<div className="col-xs-offset-2 col-xs-8">
-						<Header />
-					</div>
-				</div>
-				<div className="row">
-					<div className="col-xs-2 col-xs-offset-2">
-						<div className="list-group">
-							<MyNavLink  to='/home' title='Home' children='Home' />
-							<MyNavLink  to='/about' title='About' a={1} children='About' />
-						</div>
-					</div>
-					<div className="col-xs-6">
-						<div className="panel">
-							<div className="panel-body">
-								{/* 注册路由 */}
-								<Switch>
-									<Route path='/home' component={Home} />
-									<Route path='/about' component={About} />
-									<Redirect to='/about'/>{/*Redirect为重定向，兜底的  */}
-								</Switch>
-							</div>
-						</div>
-					</div>
-				</div>
+				App
+				<button>点我...</button>
+				<Button type="primary">按钮1</Button>
+				<Button >按钮2</Button>
+				<Button type="link">按钮3</Button>
+				<Button type="primary" icon={<SearchOutlined />}>Search</Button>
+				<WechatOutlined />
+				<WeiboOutlined />
+				<DatePicker onChange={this.onChange} />
+				<RangePicker/>
 			</div>
 		)
 	}
--- react/react_staging/src/index.js
+++ react/react_staging/src/index.js
@@ -2,14 +2,7 @@
 import React from 'react'
 //引入ReactDOM
 import ReactDOM from 'react-dom'
-//引入react-router-dom库
-import { BrowserRouter } from 'react-router-dom'
 //引入App
 import App from './App'
 
-ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
-    document.getElementById('root')
-)
+ReactDOM.render(<App />,document.getElementById('root'))
