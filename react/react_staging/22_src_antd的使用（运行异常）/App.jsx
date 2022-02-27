6bf330bf481713b15263ca197ad221a0e61b7ca1
pw <1040479402@qq.com>
2022-02-27 14:11:55 +0800

    20220227

---
+++ react/react_staging/22_src_antd的使用（运行异常）/App.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { DatePicker, Button } from 'antd';
+import { WechatOutlined, WeiboOutlined, SearchOutlined } from '@ant-design/icons';
+const { RangePicker } = DatePicker;
+
+export default class App extends Component {
+	onChange = (date, dateString) => {
+		console.log(date, dateString);
+		return 
+	}
+	render() {
+		return (
+			<div>
+				App
+				<button>点我...</button>
+				<Button type="primary">按钮1</Button>
+				<Button >按钮2</Button>
+				<Button type="link">按钮3</Button>
+				<Button type="primary" icon={<SearchOutlined />}>Search</Button>
+				<WechatOutlined />
+				<WeiboOutlined />
+				<DatePicker onChange={this.onChange} />
+				<RangePicker/>
+			</div>
+		)
+	}
+}
--- react/react_staging/Readme.md
+++ react/react_staging/Readme.md
@@ -174,3 +174,20 @@
          }),
         );
        4.备注：不用在组件里亲自引入样式了，即：import 'antd/dist/antd.css'应该删掉
+
+## 十五、redux
+
+      学习文档
+         1.英文文档: https://redux.js.org/
+         2.中文文档: http://www.redux.org.cn/
+         3.Github: https://github.com/reactjs/redux
+
+      redux是什么
+         1.redux是一个专门用于做状态管理的JS库(不是react插件库)
+         2.它可以用在react, angular, vue等项目中, 但基本与react配合使用
+         3.作用: 集中式管理react应用中多个组件共享的状态
+
+      什么情况下需要使用redux
+         1.某个组件的状态，需要让其他组件可以随时拿到（共享）
+         2.一个组件需要改变另一个组件的状态（通信）
+         3.总体原则：能不用就不用, 如果不用比较吃力才考虑使用
